0f9c9ff42da24aaf89cfb4a304b2144f1c18adfb
Thiago Gomes <tsg.gomes9@gmail.com>
2023-09-20 16:03:29 -0400

    feat: netlify configure

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../Header'
-import Navbar from '../Navbar'
+import NavbarArea from '../Navbar'
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <Navbar />
+      <NavbarArea />
       <Outlet />
     </>
   )
