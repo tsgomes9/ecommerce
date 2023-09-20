b97fc4a3c7d466e663555c57b19ac2f0c9e43b34
Thiago Gomes <tsg.gomes9@gmail.com>
2023-09-20 16:12:49 -0400

    fix: navbar import

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../Header'
-import { NavbarArea } from '../Navbar'
+// import { NavbarArea } from '../Navbar'
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <NavbarArea />
+      {/* <NavbarArea /> */}
       <Outlet />
     </>
   )
